c1af8af1c020da1b4486a05b5b5c18d55cb9d1c8
liuxiu842 <59761421+liuxiu842@users.noreply.github.com>
2020-02-29 18:20:30 +0800

    Add files via upload

---
+++ svd_image_lxL6.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.linalg import svd
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+# 取前k个特征，对图像进行还原
+def get_image_feature(s, k):
+    # 对于S，只保留前K个特征值
+    s_temp = np.zeros(s.shape[0])
+    print("s_temp1 is :", s_temp)
+    s_temp[0:k] = s[0:k]
+    print("s_temp2 is :", s_temp)
+    s = s_temp * np.identity(s.shape[0])
+    print("s is:",s)
+    # 用新的s_temp，以及p,q重构A
+    temp = np.dot(p,s)
+    temp = np.dot(temp,q)
+    plt.imshow(temp, cmap=plt.cm.gray, interpolation='nearest')
+    plt.show()
+    print("temp is", temp)
+    print("A-temp is", A-temp)
+    print("A is", A)
+
+
+# 加载256色图片
+image = Image.open('./new.png')
+A = np.array(image)
+print("A is :", A.shape)
+# 显示原图像
+plt.imshow(A, cmap=plt.cm.gray, interpolation='nearest')
+plt.show()
+# 对图像矩阵A进行奇异值分解，得到p,s,q
+p,s,q = svd(A, full_matrices=False)
+print("s is :", s)
+print("p is :", p)
+print("q is :", q)
+# 取前k个特征，对图像进行还原
+#get_image_feature(s, 5)
+get_image_feature(s, 50)
+get_image_feature(s, 500)
+#get_image_feature(s, 1000)
+
